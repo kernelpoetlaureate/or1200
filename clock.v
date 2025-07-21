9ad99b57b756080fb6957471a274cca3e5e2e67f
OmniMan <209760780+OmniManAintGotNothingOnMe@users.noreply.github.com>
2025-07-21 16:54:51 +0400

    Create clock.v

---
+++ clock.v
@@ -0,0 +1,11 @@
+module clock_generator;
+    reg clk;
+
+    initial begin
+        clk = 0; // Initialize clock to 0
+    end
+
+    always begin
+        #5 clk = ~clk; // Toggle clock every 5 time units (10 time units for a full cycle)
+    end
+endmodule
